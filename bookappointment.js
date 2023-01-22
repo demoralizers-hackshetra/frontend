7a3a94f52951c98a1846c8b498a2598593d1d73b
Bhavay Goel <goelbhavay@gmail.com>
2023-01-22 07:49:44 +0530

    fix bookappointment

--- bookappointment.js
+++ bookappointment.js
@@ -9,14 +9,14 @@ const price = localStorage.getItem('price');
 const appname = localStorage.getItem('appname');
 const appid = localStorage.getItem('apptype');
 
-function parseJwt (token) {
-    var base64Url = token.split('.')[1];
-    var base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
-    var jsonPayload = decodeURIComponent(atob(base64).split('').map(function(c) {
-        return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
-    }).join(''));
+function parseJwt(token) {
+  var base64Url = token.split('.')[1];
+  var base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
+  var jsonPayload = decodeURIComponent(atob(base64).split('').map(function (c) {
+    return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
+  }).join(''));
 
-    return JSON.parse(jsonPayload);
+  return JSON.parse(jsonPayload);
 };
 
 // Check if user is logged in
@@ -39,71 +39,76 @@ if (!jwt) {
   document.getElementById('appointment-form').classList.remove('hidden');
 
   // Handle form submission
-  document.getElementById('submit-button').addEventListener('click', e => {
+  document.getElementById('slotBtn').addEventListener('click', e => {
     e.preventDefault();
 
     // Get form data
     const date = document.getElementById('date').value;
-    const time = document.getElementById('time').value;
     const interactionMethod = document.getElementById('interaction-method').value;
-    const datetime = date + ' ' + time + ":00";
+    // const datetime = date + ' ' + time + ":00";
 
     fetch('http://127.0.0.1:3000/doctor/timeslots', {
       method: 'POST',
       headers: {
-        
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify({
-        doctor_id: docid,
-        date: "2023-01-21"
-      })
-    })
-    .then(response=>response.json())
-    .then(response => {
-      // Handle success/error
-      // window.location = "/myappointments.html";
-      var e = length(response);
-    })
-    .catch(error => {
-      console.error(error);
-      
-    });
-  });
-   
-  function myFunction(e){
-    for(let i = 0; i< e; i++){
-      let newBtn = document.createElement('button');
-      newBtn.innerText="Click me";
-      document.getElementById('button-container').appendChild(newBtn);
-    }
-  }
 
-    // Make API call to http://127.0.0.1:3000/newappointment
-    fetch('http://127.0.0.1:3000/newappointment', {
-      method: 'POST',
-      headers: {
-        'Authorization': jwt,
         'Content-Type': 'application/json'
       },
       body: JSON.stringify({
         doctor_id: docid,
-        patient_id: patid,
-        datetime: datetime,
-        phyorvirt: interactionMethod,
-        status: "scheduled",
-        prescription: "",
-        apptype: appid
+        date: date
       })
     })
+      .then(response => response.json())
+      .then(response => {
+        console.log(response);
+        // Handle success/error
+        // window.location = "/myappointments.html";
+        for (i in response) {
+          console.log(response[i]);
+          let newBtn = document.createElement('button');
+          newBtn.classList.add('slotBtn');
+          newBtn.value = response[i].slot_id;
+          newBtn.innerText = response[i].time_start;
+          if(response[i].available == false){
+            newBtn.disabled = true;
+          } else {
+            newBtn.addEventListener('click', e => makeApt(e));
+          }
+          document.getElementById('button-container').appendChild(newBtn);
+        }
+      })
+      .catch(error => {
+        console.error(error);
+
+      });
+  });
+
+function makeApt(e){
+  // Make API call to http://127.0.0.1:3000/newappointment
+  fetch('http://127.0.0.1:3000/newappointment', {
+    method: 'POST',
+    headers: {
+      'Authorization': jwt,
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+      doctor_id: docid,
+      patient_id: patid,
+      date: '2023-01-22',
+      phyorvirt: "physical",
+      status: "scheduled",
+      // prescription: "",
+      apptype: appid
+    })
+  })
     .then(response => {
       // Handle success/error
-      window.location = "/myappointments.html";
+      //window.location = "/myappointments.html";
     })
     .catch(error => {
       console.error(error);
     });
-  });
+
 }
 
-
+}
